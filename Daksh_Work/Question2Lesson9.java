01caf90a5e562d13ab7e53081293b44c191afed3
DakshAsishCSA <daksh.asish@gmail.com>
2026-09-10 23:25:07 +0000

    Homework for lesson 9

---
+++ Daksh_Work/Question2Lesson9.java
@@ -0,0 +1,16 @@
+public class Question2Lesson9{
+    
+    public static void main(String[] args){
+        String frndName = "Ryker";
+        int luckyNumber = 1;
+        boolean morningPerson = false;
+        String favoriteSnack = "Cookies" ;
+        double price = 0.50;
+        System.out.println( frndName);
+        System.out.println( luckyNumber);
+        System.out.println( morningPerson);
+        System.out.println( favoriteSnack);
+        System.out.println( price);
+    }
+
+}
